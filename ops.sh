59f0806450d2ff948a0c93ec1ff5bb4638e7dd22
mujahed85 <mujahed.mca@gmail.com>
2024-10-06 16:32:37 +0530

    V4: added new version

--- ops.sh
+++ ops.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # VERSION_NUMBER="v3"
-cd /root/PyFlaskDocker/
+cd /root/PyFlaskDockerApp/
 git pull
-docker build -t custom-img-pyapp:v3 .
-docker run --rm -p 8000:8000 custom-img-pyapp:v3
+docker build -t custom-img-pyapp:v4 .
+docker run --rm -p 8000:8000 custom-img-pyapp:v4
