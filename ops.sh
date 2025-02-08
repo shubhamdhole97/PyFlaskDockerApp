5cd9db8276279c43dfc667f7b7b43a2248d81e19
Syed Mujahed Hussaini <mujahed.mca@gmail.com>
2025-02-08 12:36:16 +0600

    Update ops.sh

--- ops.sh
+++ ops.sh
@@ -2,5 +2,5 @@
 # VERSION_NUMBER="v3"
 cd /root/PyFlaskDockerApp/
 git pull
-docker build -t custom-img-pyapp:v4 .
+docker build -t custom-img-pyapp:$(cat version.txt) .
 docker run --rm -p 8000:8000 custom-img-pyapp:v4
